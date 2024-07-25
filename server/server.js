c7a74f5d82cffe6f665eb9c167accd7a9e529b27
Stormtrooper7652 <145193717+Stormtrooper7652@users.noreply.github.com>
2024-07-25 20:53:46 +0200

    Added db file and changed node api

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
 import express from "express";
+import fs from "fs";
 const app = express();
+const __dirname = import.meta.dirname;
 
 app.get("/passwords", (req, res) => {
-    res.end("chuckle nuts")
+    fs.readFile(`${__dirname}/../db/osu.json`, 'utf8', (err, data) => {
+        try {
+            res.end(JSON.stringify(JSON.parse(data)))
+        } catch (error) {
+            res.end("Sorry, failed to fetch")
+        }
+    })
 })
 
 app.listen(8989, () => {
