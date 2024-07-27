cd2a05d22069856a2cc61f0413ff3116b62417cf
Stormtrooper7652 <145193717+Stormtrooper7652@users.noreply.github.com>
2024-07-27 16:59:50 +0200

    Changed api address

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const visitEl = document.getElementById('visit-number')
 
 window.onload = async () => {
-    // const response = await fetch('https://server.osudb.online/visit', { mode: 'no-cors' })
-    // const response = await fetch('https://server.osudb.online/visit')
-    const response = await fetch('http://127.0.0.1:8989/visit')
+    const response = await fetch('https://server.osudb.online/visit')
     const data = await response.json()
     visitEl.innerText = data.count
 }
